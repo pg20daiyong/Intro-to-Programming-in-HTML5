09d4e6b409946791882132e57fbc8aaa2f5a0dd0
Daiyong Kim <pg20daiyong@vfs.com>
2021-02-19 21:58:57 -0800

    temporary delete main home page

--- orchard/scripts/Orange.js
+++
@@ -1,8 +0,0 @@
-// copyright (c) 2021 Daiyong Kim
-'use strict'
-import Fruit from "./Fruit.js";
-export default class Orange extends Fruit{
-    constructor(theSize = 4, seedCount = 0) {
-                super(theSize, seedCount, "Orange");
-    }
-}
